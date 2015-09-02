2a3cff8a69989905b6e5f29093da5b04176355b1
Jared Noble <jsnoble1@gmail.com>
2015-09-02 16:23:47 -0700

    changed makeLogger to make_logger file name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (config) {
     var primary = domain.create();
     var cluster = require('cluster');
     var _ = require('lodash');
-    var makeLogger = require('./lib/makeLogger');
+    var makeLogger = require('./lib/make_logger');
     var startWorkers = require('./lib/start_workers');
 
     var argv = require('yargs')
