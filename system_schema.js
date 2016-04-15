2fbf745365cb4aa816ea67099319d9a0f6ccfb52
Jared Noble <jsnoble1@gmail.com>
2016-04-15 08:25:12 -0700

    fixed schema for changes for logging

--- system_schema.js
+++ system_schema.js
@@ -1,17 +1,24 @@
 'use strict';
 var workerCount = require('os').cpus().length;
 
+var dirPath = process.cwd();
+
 module.exports = {
     environment: {
         doc: '',
         default: 'development'
     },
     log_path: {
-        doc: '',
-        default: '/Users/jarednoble/Desktop/logs'
+        doc: 'directory where the logs will be stored in logging is set to file',
+        default: dirPath
+    },
+    logging: {
+        doc: 'options to specify which logging functionality to use',
+        default: 'console',
+        format: ['console', 'file', 'elasticsearch']
     },
     workers: {
-       doc: 'Number of workers per server',
+        doc: 'Number of workers per server',
         default: workerCount
     }
 
